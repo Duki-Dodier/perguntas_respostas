5621b298bafd002fb6b02670afe6524c2692f389
DukiDodier <duki.dodier@gmail.com>
2022-03-07 15:02:37 -0300

    Exibindo variaveis no hTML

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ const app = express();
 // Configura o express para usar o EJS com View Engine
 app.set("view engine", "ejs");
 
-app.get("/", (request, response) => {
-  response.render("index");
+app.get("/:nome", (request, response) => {
+    const nome = request.params.nome;
+    
+    const objetu = {sobrenome:"andrade", idade:32}
+  response.render("index",{nome,objetu});
 });
 
 const port = 3000;
