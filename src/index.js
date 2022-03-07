e706836c1d58debe1a05499f392c2e51399629e0
DukiDodier <duki.dodier@gmail.com>
2022-03-07 14:52:51 -0300

    Configurando e exibindo HTML com EJS

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const express = require("express");
 
 const app = express();
 
+// Configura o express para usar o EJS com View Engine
+app.set("view engine", "ejs");
+
 app.get("/", (request, response) => {
-  response.send("bem vindo");
+  response.render("index");
 });
 
-const port = 3000
-app.listen(port, function(e){
+const port = 3000;
+app.listen(port, function (e) {
   if (e) console.error(`ERROR SERVIDOR >>> ${e}`);
   console.log("ServidorRodando 🚀");
 });
